b0388793402ed4157f4026c36e68c2dcddbd301a
Asaniczka <asaniczka@gmail.com>
2024-08-10 02:57:53 +0530

    IMRPOVE: organization

--- README.md
+++ README.md
@@ -1,30 +1,62 @@
-# Upwork Job Dataset Maker
+# Upwork Job Scraper
 
-This project aims to create a large dataset of job postings on Upwork for future analysis.
+Welcome to the Upwork Job Scraper! This is a microservice-based project designed to fetch job postings from Upwork, filter them, and enrich them with additional useful information.
 
-## Task description
+## Table of Contents
 
-- Fetch RSS feeds from a specified URL every 2 minutes.
-- Save the fetched RSS feeds as files in a "collections" folder.
-- Parse the XML files and extract job information.
-- Process the job descriptions to extract specific details such as hourly rates, budgets, and country.
-- Create a pandas dataframe from the extracted job data.
-- Remove duplicate job listings based on the job link.
-- Save the dataset as a CSV file.
+- [Overview](#overview)
+- [Architecture](#architecture)
+- [Modules](#modules)
+- [Job Fetcher](#job-fetcher)
+- [Data Enricher](#data-enricher)
 
-## Key Responsibilities 
+## Overview
 
-- Ensure that the RSS feeds are fetched and saved correctly.
-- Handle any errors that occur during the parsing or processing of job data.
-- Pay attention to the correct extraction of job details from the descriptions.
-- Remove duplicate job listings to maintain data integrity.
-- Save the dataset in the proper format and location.
+The Upwork Job Scraper consists of two main modules: Job Fetcher and Data Enricher. The Job Fetcher is responsible for retrieving job postings from Upwork and storing them in a PostgreSQL database. The Data Enricher module enhances the filtered job data with additional contextual or relevant information, making it more valuable and actionable.
 
-## How to Use
+## Architecture
 
-1. Clone this repository to your local machine.
-2. Install the required dependencies by running `pip install -r requirements.txt`.
-3. Set the URL for the RSS feeds in the `.env` file.
-4. Run the `collector.py` script using `python collector.py` to start collecting the RSS feeds.
-5. After collecting a sufficient number of XML files, run the `dataset_maker.py` script using `python dataset_maker.py`.
-6. The dataframe will be saved as a CSV file in the project's data folder with a timestamp.
+The architecture of the Upwork Job Scraper is designed to be modular and scalable.
+
+- **Job Fetcher**: Runs on AWS Lambda and retrieves job postings from Upwork regularly.
+- **Data Enricher**: Executes on a private server using Selenium, fetching and combining data from various sources to enrich the job postings stored in PostgreSQL.
+
+```
+                   +-------------------+
+                   |  Upwork Platform  |
+                   +-------------------+
+                             |
+                             v
+                   +-------------------+
+                   |    Job Fetcher    | <--- AWS Lambda
+                   +-------------------+
+                             |
+                             v
+                   +-------------------+
+                   |   PostgreSQL DB   |
+                   +-------------------+
+                             |
+                             |
+                             v
+                   +-------------------+
+                   |   Data Enricher   | <--- Private Server (Selenium)
+                   +-------------------+
+                             |
+                             v
+                   +-------------------+
+                   | Enriched Job Data  |
+                   +-------------------+
+```
+
+## Modules
+
+### Job Fetcher
+
+- **Functionality**: This module is responsible for fetching all jobs posted to Upwork.
+- **Storage**: The raw job data is stored in a PostgreSQL database.
+- **Filtering**: Filters the raw job data according to defined criteria and saves the filtered jobs to a separate table.
+
+### Data Enricher
+
+- **Functionality**: This module pulls the filtered jobs from the PostgreSQL database and enriches them with additional data.
+- **Tech Stack**: Utilizes Selenium for data fetching and manipulation.
---
+++ src/job_fetcher/ETL_lambda.py
@@ -0,0 +1,230 @@
+"""
+This module is responsible for collecting RSS XML feeds every 2 minutes, transforming the raw data, and loading them into the DB
+"""
+
+import os
+from datetime import datetime
+import asyncio
+import json
+from rich import print
+
+from pydantic import (
+    BaseModel,
+    Field,
+    AliasChoices,
+    AliasPath,
+    model_validator,
+    field_serializer,
+    field_validator,
+)
+import httpx
+import ua_generator
+
+# import dotenv
+# dotenv.load_dotenv()
+
+
+class OntologySkill(BaseModel):
+    """Holds and individual skill"""
+
+    prefLabel: str
+
+
+class Job(BaseModel):
+    """Holds data for an individual job"""
+
+    title: str | None = Field(validation_alias=AliasPath("title"))
+    link: str = Field(validation_alias=AliasPath("jobTile", "job", "ciphertext"))
+    description: str | None = Field(validation_alias=AliasPath("description"))
+    skills: list[OntologySkill] | None = Field(
+        validation_alias=AliasPath("ontologySkills")
+    )
+    published_date: datetime = Field(
+        validation_alias=AliasPath("jobTile", "job", "publishTime")
+    )
+    is_hourly: bool = None
+    job_type: str = Field(validation_alias=AliasPath("jobTile", "job", "jobType"))
+    hourly_low: int | str | None = Field(
+        validation_alias=AliasPath("jobTile", "job", "hourlyBudgetMin")
+    )
+    hourly_high: int | str | None = Field(
+        validation_alias=AliasPath("jobTile", "job", "hourlyBudgetMax")
+    )
+    budget: int | str | None = Field(
+        validation_alias=AliasChoices(
+            AliasPath("jobTile", "job", "fixedPriceAmount", "amount"),
+            AliasPath("jobTile", "job", "fixedPriceAmount"),
+        )
+    )
+
+    @model_validator(mode="after")
+    def _find_hourly(self):
+        if self.job_type == "HOURLY":
+            self.is_hourly = True
+        elif self.job_type == "FIXED":
+            self.is_hourly = False
+        else:
+            self.is_hourly = None
+
+        return self
+
+    @field_validator("hourly_low", "hourly_high", "budget", mode="after")
+    @classmethod
+    def _str2int(cls, value):
+
+        if value is None:
+            return None
+
+        return int(float(value))
+
+    @field_serializer("skills", when_used="always")
+    @classmethod
+    def _ontology2str(cls, value: list[OntologySkill]):
+
+        if not value:
+            return None
+
+        return ", ".join(x.prefLabel for x in value)
+
+    @field_validator("link", mode="after")
+    @classmethod
+    def _cypher2link(cls, value: str):
+
+        return "https://www.upwork.com/freelance-jobs/apply/" + value
+
+
+class JobList(BaseModel):
+    """Holds a list of job posts"""
+
+    jobs: list[Job] = Field(
+        validation_alias=AliasPath(
+            "data", "search", "universalSearchNuxt", "visitorJobSearchV1", "results"
+        )
+    )
+
+
+def collect_jobs() -> dict | None:
+    """"""
+
+    url = "https://www.upwork.com/api/graphql/v1"
+
+    payload = {
+        "query": """
+            query VisitorJobSearch($requestVariables: VisitorJobSearchV1Request!) {
+                search {
+                universalSearchNuxt {
+                    visitorJobSearchV1(request: $requestVariables) {
+                    results {
+                        id
+                        title
+                        description
+                        ontologySkills {
+                        prefLabel
+                        }
+                        jobTile {
+                        job {
+                            id
+                            ciphertext: cipherText
+                            jobType
+                            hourlyBudgetMax
+                            hourlyBudgetMin
+                            contractorTier
+                            publishTime
+                            fixedPriceAmount {
+                            amount
+                            }
+                        }
+                        }
+                    }
+                    }
+                }
+                }
+            }
+    """,
+        "variables": {
+            "requestVariables": {
+                "sort": "recency",
+                "highlight": True,
+                "paging": {"offset": 0, "count": 50},
+            }
+        },
+    }
+    headers = {
+        "User-Agent": str(ua_generator.generate()),
+        "Accept": "*/*",
+        "Accept-Language": "en-GB,en;q=0.7,en-US;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Referer": "https://www.upwork.com/nx/search/jobs/",
+        "X-Upwork-Accept-Language": "en-US",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer x",  # TODO: get token
+        "Origin": "https://www.upwork.com",
+        "DNT": "1",
+        "Connection": "keep-alive",
+    }
+
+    response = httpx.post(url, json=payload, headers=headers)
+
+    if response.status_code != 200:
+        print(response.text)
+
+    return response.json()
+
+
+async def upload_to_db(job: Job, client: httpx.Client):
+    """Uploads to db"""
+
+    anon_key = os.getenv("sb_anon_key")
+
+    try:
+        url = "https://aklwouiknrlbwnqazyht.supabase.co/rest/v1/upwork_jobs_streaming"
+        headers = {
+            "apikey": anon_key,
+            "Authorization": f"Bearer {anon_key}",
+            "Content-Type": "application/json",
+            "Prefer": "return=minimal",
+        }
+
+        response = await client.post(
+            url,
+            headers=headers,
+            data=job.model_dump_json(exclude="job_type"),
+            timeout=3,
+        )
+
+        print(response.status_code, response.text)
+    except Exception as error:
+        print(error)
+
+
+async def handle_load(jobs: list[Job]):
+    """handles uploading in parallel"""
+
+    client = httpx.AsyncClient()
+
+    tasks = []
+    for job in jobs:
+        tasks.append(upload_to_db(job, client))
+
+    await asyncio.gather(*tasks)
+
+
+def lambda_handler(event, context):
+    """
+    handles the process of a single rss feed
+    """
+
+    raw_feed = collect_jobs()
+
+    if not raw_feed:
+        return {"statusCode": 500, "body": json.dumps("Unable to extract from upwork")}
+
+    jobs = JobList(**raw_feed)
+    print(jobs.model_dump())
+
+    # asyncio.run(handle_load(jobs))
+    # return {"statusCode": 200, "body": json.dumps("All Good")}
+
+
+if __name__ == "__main__":
+    lambda_handler(1, 1)
